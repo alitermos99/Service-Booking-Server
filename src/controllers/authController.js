4731f826759756f635113e0b6aca40ef8045c341
Ali Termos <ali1999.termos@hotmail.com>
2026-05-23 19:00:00 +0300

    build(v1): added the token to httponly cookie in authController.js , created login and logout functions and imported the cookie-parser in index.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 
 const generateToken = (user) => {
 	return jwt.sign(
@@ -12,6 +13,10 @@ const generateToken = (user) => {
 export const register = async (req, res) => {
 	try {
 		const { name, email, password, phone, role } = req.body;
+
+		if(!name || !email || !password) {
+			return res.status(400).json({ message: "Name, email, and password are required" });
+		}
 
 		// Check if user already exists
 		const existingUser = await User.findOne({ email });
@@ -33,13 +38,74 @@ export const register = async (req, res) => {
 		const newUserNoPassword = newUser.toObject();
 		delete newUserNoPassword.password;
 
-		res.status(201).json({ 
-			message: "User registered successfully", 
-			user: newUserNoPassword, 
-			token: generateToken(newUser) 
+		const token = generateToken(newUser);
+
+		res.cookie("token", token, {
+			httpOnly: true,
+			secure: false, // true in production (HTTPS)
+			sameSite: "strict",
+			maxAge: 24 * 60 * 60 * 1000 // 1 day
+		});
+
+		res.json({
+			message: "Login successful",
+			token,
+			newUserNoPassword
 		});
 	} catch (error) {
 		console.error("Registration error:", error);
 		res.status(500).json({ message: "Server error during registration" });
 	}
 };
+
+export const login = async (req, res) => {
+	try {
+		const { email, password } = req.body;
+		
+		if(!email || !password) {
+			return res.status(400).json({ message: "Email and password are required" });
+		}
+
+		// Check if user exists
+		const user = await User.findOne({ email });
+
+		if (!user) {
+			return res.status(400).json({ message: "Invalid email or password" });
+		}
+
+		const isMatch = await bcrypt.compare(password, user.password);
+
+		if (!isMatch) {
+			return res.status(400).json({ message: "Invalid email or password" });
+		}
+
+		const token = generateToken(user);
+
+		res.cookie("token", token, {
+			httpOnly: true,
+			secure: process.env.environment === "production", // true in production (HTTPS)
+			sameSite: "strict",
+			maxAge: 24 * 60 * 60 * 1000 // 1 day
+		});
+		
+		res.json({ token, user: {
+			name: user.name,
+			email: user.email,
+			phone: user.phone
+		}});
+	} catch (error) {
+		console.error("Login error:", error);
+		res.status(500).json({ message: "Server error during login" });
+	}
+}
+
+export const logout = async (req, res) => {
+	res.clearCookie("token", {
+		httpOnly: true,
+		secure: process.env.environment === "production",
+		sameSite: "strict",
+		maxAge: 0
+	});
+
+	res.json({ message: "Logged out successfully" });
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from "dotenv";
 import connectDB from './config/db.js';
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
 dotenv.config();
 
 // Connect to MongoDB
@@ -12,7 +15,8 @@ app.get('/', (req, res) => {
 	res.send('Hello from Node.js server!');
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
 });
